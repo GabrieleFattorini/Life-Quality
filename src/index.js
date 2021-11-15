6a46bbc735947cf5edb2748a42ab805bcffac7ec
GabrieleFattorini <86602631+GabrieleFattorini@users.noreply.github.com>
2021-11-15 13:45:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,57 @@
 const apiKey = process.env.API_KEY;
 
-let pollution = {
-    fetchPollution: function (city) {
+let lifeQuality = {
+    fetchLifeQuality: function (city) {
         fetch(
-            "http://api.waqi.info/feed/" + city + "/?token=" + apiKey
+            apiKey + city + "/scores/"
             ).then((response) => response.json())
-            .then((data) => this.displayPollution(data));
+            .then((data) => this.displayLifeQuality(data));
     },
-    fetchPosition: function () {
-        fetch(
-            "http://api.waqi.info/feed/here/?token=" + apiKey
-            ).then((response) => response.json())
-            .then((data) => this.displayPollution(data));
-    },
-    displayPollution: function({data}) {
-        const { idx, aqi } = data;
-        const { name } = data.city;
-        const { s } = data.time;
-        const { avg, day, max, min } = data.forecast.daily.pm25[0];
+    displayLifeQuality: function(data) {
+        const { teleport_city_score, summary } = data;
+        const { score_out_of_10 : cat1 } = data.categories[0];
+        const { score_out_of_10 : cat2 } = data.categories[1];
+        const { score_out_of_10 : cat3 } = data.categories[2];
+        const { score_out_of_10 : cat4 } = data.categories[3];
+        const { score_out_of_10 : cat5 } = data.categories[4];
+        const { score_out_of_10 : cat6 } = data.categories[5];
+        const { score_out_of_10 : cat7 } = data.categories[6];
+        const { score_out_of_10 : cat8 } = data.categories[7];
+        const { score_out_of_10 : cat9 } = data.categories[8];
+        const { score_out_of_10 : cat10 } = data.categories[9];
+        const { score_out_of_10 : cat11 } = data.categories[10];
+        const { score_out_of_10 : cat12 } = data.categories[11];
+        const { score_out_of_10 : cat13 } = data.categories[12];
+        const { score_out_of_10 : cat14 } = data.categories[13];
         
-        document.querySelector(".city span").innerText = name;
-        document.querySelector(".idx span").innerText = idx;
-        document.querySelector(".aqi span").innerText = aqi;
-        document.querySelector(".time span").innerText = s;
-        document.querySelector(".forecast span").innerText = day + ' ( avg: ' + avg + ', max: ' + max + ', min: ' + min + ' )';
+        document.querySelector(".score span").innerText = `${document.querySelector('.search-bar').value} ${parseInt(teleport_city_score)}`;
+        document.querySelector(".summary").innerText = summary;
+        document.querySelector(".housing span").innerText = `${parseInt(cat1)} / 10`;
+        document.querySelector(".living span").innerText = `${parseInt(cat2)} / 10`;
+        document.querySelector(".startups span").innerText = `${parseInt(cat3)} / 10`;
+        document.querySelector(".capital span").innerText = `${parseInt(cat4)} / 10`;
+        document.querySelector(".connectivity span").innerText = `${parseInt(cat5)} / 10`;
+        document.querySelector(".commute span").innerText = `${parseInt(cat6)} / 10`;
+        document.querySelector(".business span").innerText = `${parseInt(cat7)} / 10`;
+        document.querySelector(".safety span").innerText = `${parseInt(cat8)} / 10`;
+        document.querySelector(".healthcare span").innerText = `${parseInt(cat9)} / 10`;
+        document.querySelector(".education span").innerText = `${parseInt(cat10)} / 10`;
+        document.querySelector(".quality span").innerText = `${parseInt(cat11)} / 10`;
+        document.querySelector(".economy span").innerText = `${parseInt(cat12)} / 10`;
+        document.querySelector(".taxation span").innerText = `${parseInt(cat13)} / 10`;
+        document.querySelector(".internet span").innerText = `${parseInt(cat14)} / 10`;
     },  
     search: function() {
-        this.fetchPollution(document.querySelector('.search-bar').value);
+        this.fetchLifeQuality(document.querySelector('.search-bar').value);
     },
 };
 
 document.querySelector('.search button').addEventListener('click', function() {
-    pollution.search();
+    lifeQuality.search();
 });
 
 document.querySelector('.search-bar').addEventListener('keyup', function(event) {
     if (event.key == "Enter") {
-        pollution.search();
+        lifeQuality.search();
     };
 });
-
-window.addEventListener('load', () => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log('Geolocation is not allowed');
-    }
-});
-
-function showPosition() {
-    pollution.fetchPosition();
-}
